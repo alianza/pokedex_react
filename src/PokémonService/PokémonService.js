6a433c777621e14486e4fc0d3d72ffd237f76be8
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-24 04:08:14 +0100

    Added loadTypePokémons function

--- src/PokémonService/PokémonService.js
+++ src/PokémonService/PokémonService.js
@@ -6,31 +6,43 @@ const PokémonService = {
         if (!url.includes(baseUrl)) {
             url = baseUrl + url
         }
-
+        // console.log(url)
         return fetch(url)
             .then(response => response.json())
-            .then(data => { console.log(data); return data; });
+            // .then(data => {console.log(data)})
+            .then( data => {return data});
     },
+
     loadPokemons() {
         return this.doLoad('/pokemon').then(jsonData => {
             return jsonData
         }).catch(e => { console.log('Error', e) });
     },
+
     loadPokémon(pokémonName) {
         return this.doLoad(`/pokemon/${pokémonName}`).then(jsonData => {
             return jsonData
         }).catch(e => { console.log('Error', e) });
     },
+
     getTotalNumberOfPokemon() {
         return this.doLoad('/pokemon-species/?limit=0').then(jsonData => {
             return jsonData
         }).catch(e => { console.log('Error', e) });
     },
+
     loadTypes() {
         return this.doLoad('/type/').then(jsonData => {
             return jsonData
         }).catch(e => { console.log('Error', e) });
     },
+
+    loadTypePokémons(type) {
+        return this.doLoad(`/type/${type}`).then(jsonData => {
+            return jsonData
+        }).catch(e => { console.log('Error', e) });
+    },
+
     loadRandomPokemon(randomIndex) {
         return this.doLoad('/pokemon/' + randomIndex).then(jsonData => {
             return jsonData
