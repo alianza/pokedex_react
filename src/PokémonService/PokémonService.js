09f6d7a89681e0202e0810e4a8c846cfc65f7b8e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-24 22:31:19 +0100

    Added totalNumberOfPokémon caching, and basePageLimit prop for pagination

--- src/PokémonService/PokémonService.js
+++ src/PokémonService/PokémonService.js
@@ -1,56 +1,69 @@
-const baseUrl = "https://pokeapi.co/api/v2";
 
 const PokémonService = {
-    doLoad(url) { // Base method for doing http Get requests
+    baseUrl: "https://pokeapi.co/api/v2",
+    basePageLimit: 20,
+    totalNumberOfPokémon: 0,
 
-        if (!url.includes(baseUrl)) {
-            url = baseUrl + url
+    doLoad(url) { // Base method for doing http Get requests
+        if (!url.includes(this.baseUrl)) {
+            url = this.baseUrl + url;
         }
+
         // console.log(url)
         return fetch(url)
             .then(response => response.json())
-            // .then(data => {console.log(data)})
-            .then( data => {return data});
+            .then(data => {
+                // console.log(data);
+                return data})
     },
 
-    getPokemons() {
+    getPokémons() {
         return this.doLoad('/pokemon').then(jsonData => {
-            return jsonData
+            return jsonData;
+        }).catch(e => { console.log('Error', e) });
+    },
+
+    getPagedPokémons(offset) {
+        return this.doLoad(`/pokemon?offset=${offset}&limit=${this.basePageLimit}`).then(jsonData => {
+            return jsonData;
         }).catch(e => { console.log('Error', e) });
     },
 
     getPokémon(pokémonName) {
         return this.doLoad(`/pokemon/${pokémonName}`).then(jsonData => {
-            return jsonData
+            return jsonData;
         }).catch(e => { console.log('Error', e) });
     },
 
     getTypes() {
         return this.doLoad('/type/').then(jsonData => {
-            return jsonData
+            return jsonData;
         }).catch(e => { console.log('Error', e) });
     },
 
     getTypePokémons(type) {
         return this.doLoad(`/type/${type}`).then(jsonData => {
-            return jsonData
+            return jsonData;
         }).catch(e => { console.log('Error', e) });
     },
 
     getRandomPokémon() {
-        return this.getTotalNumberOfPokémon().then(json => {
-            const totalNumberOfPokémon = json.count
-            const randomIndex = Math.floor(Math.random() * (totalNumberOfPokémon - 1)) + 1
-            return this.doLoad(`/pokemon/${randomIndex}`).then(jsonData => {
-                return jsonData
-            }).catch(e => { console.log('Error', e) });
-        })
+        return this.getTotalNumberOfPokémon().then(() => {
+            const randomIndex = Math.floor(Math.random() * (this.totalNumberOfPokémon - 1)) + 1;
+            return this.getPokémon(randomIndex).then(jsonData => { return jsonData; })
+                .catch(e => {console.log('Error', e)});
+        });
     },
 
     getTotalNumberOfPokémon() {
-        return this.doLoad('/pokemon-species/?limit=0').then(jsonData => {
-            return jsonData
-        }).catch(e => { console.log('Error', e) });
+        if (!this.totalNumberOfPokémon) {
+            return this.doLoad('/pokemon-species/?limit=0').then(jsonData => {
+                this.totalNumberOfPokémon = jsonData.count;
+                return jsonData;
+            }).catch(e => { console.log('Error', e) });
+        } else {
+            return new Promise(resolve => {resolve(this.totalNumberOfPokémon)})
+        }
     },
 }
 
