8c9df2d24aa0122ba56689d1f378a0a412f4ef55
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-24 05:20:43 +0100

    Added dynamic navLinks

--- src/PokéMenu/PokéMenu.js
+++ src/PokéMenu/PokéMenu.js
@@ -1,6 +1,6 @@
 import './PokéMenu.scss'
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 class PokéMenu extends React.Component<{}> {
     render() {
@@ -9,9 +9,9 @@ class PokéMenu extends React.Component<{}> {
                 <div onClick={e => this.props.onMenuClick(e)} className="menu-close">✖</div>
                 <h1>Menu</h1>
                 <ul className="menu-top">
-                    <li className="active"><Link to={'/'} onClick={this.setActiveMenuItem}>Home</Link></li>
-                    <li><Link to={'/types'} onClick={this.setActiveMenuItem}>Types</Link></li>
-                    <li><Link to={'/random'} onClick={this.setActiveMenuItem}>Random Pokemon</Link></li>
+                    <li><NavLink exact to={'/'} activeClassName={'active'}>Home</NavLink></li>
+                    <li><NavLink to={'/types'} activeClassName={'active'}>Types</NavLink></li>
+                    <li><NavLink exact to={'/random'} activeClassName={'active'}>Random Pokemon</NavLink></li>
                     <li data-item="about" onClick={this.about}>About</li>
                 </ul>
                 <p className="menu-bottom">Jan-Willem van Bremen</p>
@@ -19,11 +19,6 @@ class PokéMenu extends React.Component<{}> {
         );
     }
 
-    setActiveMenuItem(e) {
-        document.getElementsByClassName('active')[0].classList.remove('active');
-        e.target.parentElement.classList.add('active');
-    }
-    
     about = () => {
         alert('This is a Web PokéDex Application!\n' +
             'Discover countless Pokemon and their info!\n' +
