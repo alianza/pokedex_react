0c3d8aa1c3cbfd4b14eecd6faa8a8ee15f708d72
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-24 02:04:01 +0100

    Added pokémondetails

--- src/PokémonDetails/PokémonDetails.js
+++ src/PokémonDetails/PokémonDetails.js
@@ -1,65 +1,98 @@
+import './PokémonDetails.scss'
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import PokémonService from "../PokémonService/PokémonService";
+import capitalize from "../helpers/Capitalize";
+import typeToColor from "../helpers/TypeToColor";
 
 class PokémonDetails extends Component {
     render() {
         return (
             <div>
-                <div onClick={'closeDialog'} class="backdrop"/>
+                <Link to={'/'} className="backdrop"/>
 
-                <div class="details">
-                    <div onClick={"closeDialog()"} class="details-close">✖</div>
+                {this.state &&
+                <div className="details">
 
-                    <div class="details-container">
+                    <Link to={'/'} className="details-close">✖</Link>
 
-                        <div class="details-info">
-                            <h1 class="details-info-name">pokemon.name | capitalize</h1>
-                            <div class="details-info-profile">
+                    <div className="details-container">
+                        <div className="details-info">
+
+                            <h1 className="details-info-name">{capitalize(this.state.pokémon.name)}</h1>
+                            <div className="details-info-profile">
                                 <h2>Profile</h2>
                                 <dl>
                                     <dt><h4>Base Experience</h4></dt>
-                                    <dd>pokemon.base_experience }} xp</dd>
+                                    <dd>{this.state.pokémon.base_experience } xp</dd>
 
                                     <dt><h4>Height</h4></dt>
-                                    <dd>pokemon.height / 10 }} m</dd>
+                                    <dd>{this.state.pokémon.height / 10 } m</dd>
 
                                     <dt><h4>Weight</h4></dt>
-                                    <dd>pokemon.weight / 10 }} kg</dd>
+                                    <dd>{this.state.pokémon.weight / 10 } kg</dd>
                                 </dl>
                             </div>
-                            <div class="details-info-types">
+                            <div className="details-info-types">
                                 <h2>Types</h2>
-                                <div class="details-info-types-type" onClick="onTypeClick(type.type)" key="type.type.name" for="type in pokemon.types"
-                                style="{backgroundColor: $options.filters.typeToColor(type.type.name)}"> type.type.name | capitalize }}
+                                {this.state.pokémon.types.map((type) => {
+                                    return <Link key={type.type.name} to={`/type/${type.type.name}`}><div
+                                           style={{backgroundColor: typeToColor(type.type.name)}}
+                                           className="details-info-types-type">{capitalize(type.type.name)}</div></Link>})}
+                            </div>
+
+                            <div className="details-info-stats">
+                                <h2>Stats</h2>
+                                {
+                                    this.state.pokémon.stats.map((stat) => {
+                                    return <div className="details-info-stats-stat" key={stat.stat.name}>
+                                        {capitalize(stat.stat.name)}
+                                        <div style={{width: `${stat.base_stat}px`,
+                                            backgroundColor: typeToColor(this.state.pokémon.types[0].type.name)}}>
+                                            {stat.base_stat}</div>
+                                    </div>
+                                })}
+
                             </div>
                         </div>
+                        <div className="details-image">
 
-                        <div class="details-info-stats">
-                            <h2>Stats</h2>
-                            <div class="details-info-stats-stat" key="stat.stat.name" for="stat in pokemon.stats">
-                                stat.stat.name | capitalize }}
-                                <div style="{width: stat.base_stat + 'px'}"> stat.base_stat }}</div>
+                            <div id="flip-box">
+                                <div id="flip-box-inner">
+                                    <div id="flip-box-front">
+                                        <img alt="Pokemon front" src={this.state.pokémon.sprites.front_default}/>
+                                    </div>
+                                    <div id="flip-box-back">
+                                        <img alt="Pokemon back" src={this.state.pokémon.sprites.back_default}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <button onClick={this.toggleImage} className="button">↻</button>
                         </div>
                     </div>
                 </div>
-                <div class="details-image">
-
-                    <div class="flip-box">
-                        <div class="flip-box-inner">
-                            <div class="flip-box-front">
-                                <img alt="Pokemon image front" src="imageFront"/>
-                            </div>
-                            <div class="flip-box-back">
-                                <img alt="Pokemon image back" src="imageBack"/>
-                            </div>
-                        </div>
-                    </div>
-                    {/*// <!--        <img alt="pokemon image" :src="image">-->*/}
-                    <button if="!this.imageFront.includes('placeholder')" onClick="toggleImage()" class="button">↻</button>
-                </div>
+                }
             </div>
-    </div>
-    </div>
         );
+    }
+
+    componentDidMount = () => {
+        if (this.props.match.params.pokemonName) {
+            this.loadPokémon()
+        } else if (this.props.match.path === "/random") {
+            console.log("Load Random Pokémon")
+        }
+    }
+
+    loadPokémon = () => {
+        PokémonService.loadPokémon(this.props.match.params.pokemonName).then(json => {
+            this.setState({pokémon: json});
+            console.log(this.state);
+        });
+    }
+
+    toggleImage = () => {
+        document.getElementById('flip-box').classList.toggle('active');
     }
 }
 
