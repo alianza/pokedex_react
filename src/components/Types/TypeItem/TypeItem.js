c19b675aa911a7d8098a8010e21363b1f0a47ce5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-25 03:06:52 +0100

    Added scrollToTop when choosing type

--- src/components/Types/TypeItem/TypeItem.js
+++ src/components/Types/TypeItem/TypeItem.js
@@ -3,10 +3,11 @@ import React from "react";
 import capitalize from "../../../helpers/Capitalize";
 import typeToColor from "../../../helpers/TypeToColor";
 import {Link} from "react-router-dom";
+import scrollToTop from "../../../helpers/ScrollToTop";
 
 function TypeItem(props) {
     return (
-        <Link to={`/type/${props.type.name}`}>
+        <Link to={`/type/${props.type.name}`} onClick={scrollToTop}>
             <div className="type">
                 <div className="type-container" style={{backgroundColor: typeToColor(props.type.name)}}>
                     <h3 className="type-name">{capitalize(props.type.name)}</h3>
