b8f7eb60646564615ee65227880fed9a3730ec8b
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-25 01:11:33 +0100

    Indenting cleanup

--- src/PokémonDetails/PokémonDetails.js
+++ src/PokémonDetails/PokémonDetails.js
@@ -25,70 +25,69 @@ class PokémonDetails extends Component {
             <React.Fragment>
                 <div onClick={this.goBack} className="backdrop"/>
 
-                {this.state.pokémon.name &&
-                <React.Fragment>
-                    <div className="details">
-                        <div onClick={this.goBack} className="details-close">✖</div>
+    {this.state.pokémon.name &&
+        <React.Fragment>
+            <div className="details">
+                <div onClick={this.goBack} className="details-close">✖</div>
 
-                        <div className="details-container">
-                            <div className="details-info">
+                <div className="details-container">
+                    <div className="details-info">
 
-                                <h1 className="details-info-name">{capitalize(this.state.pokémon.name)}</h1>
-                                <div className="details-info-profile">
-                                    <h2>Profile</h2>
-                                    <dl>
-                                        <dt><h4>Base Experience</h4></dt>
-                                        <dd>{this.state.pokémon.base_experience} xp</dd>
+                        <h1 className="details-info-name">{capitalize(this.state.pokémon.name)}</h1>
+                        <div className="details-info-profile">
+                            <h2>Profile</h2>
+                            <dl>
+                                <dt><h4>Base Experience</h4></dt>
+                                <dd>{this.state.pokémon.base_experience} xp</dd>
 
-                                        <dt><h4>Height</h4></dt>
-                                        <dd>{this.state.pokémon.height / 10} m</dd>
+                                <dt><h4>Height</h4></dt>
+                                <dd>{this.state.pokémon.height / 10} m</dd>
 
-                                        <dt><h4>Weight</h4></dt>
-                                        <dd>{this.state.pokémon.weight / 10} kg</dd>
-                                    </dl>
-                                </div>
-                                <div className="details-info-types">
-                                    <h2>Types</h2>
-                                    {this.state.pokémon.types.map((type) => {
-                                        return <Link key={type.type.name} to={`/type/${type.type.name}`}
-                                                style={{backgroundColor: typeToColor(type.type.name)}} onClick={scrollToTop}
-                                                className="details-info-types-type">{capitalize(type.type.name)}</Link>})}
-                                </div>
-                                <div className="details-info-stats">
-                                    <h2>Stats</h2>
-                                    {this.state.pokémon.stats.map((stat) => {
-                                        return <div className="details-info-stats-stat" key={stat.stat.name}>
-                                            {capitalize(stat.stat.name)}
-                                            <div style={{
-                                                width: `${stat.base_stat}px`,
-                                                backgroundColor: typeToColor(this.state.pokémon.types[0].type.name)
-                                            }}>{stat.base_stat}</div>
-                                        </div>})}
-                                </div>
-                            </div>
-                            <div className="details-image">
-                                <div id="flip-box" className={!this.state.pokémon.sprites.back_default ? 'disabled' : ''}>
-                                    <div id="flip-box-inner">
-                                        <div id="flip-box-front">
-                                            <img alt="Pokemon front" src={this.state.pokémon.sprites.front_default}/>
-                                        </div>
-{this.state.pokémon.sprites.back_default && <div id="flip-box-back">
-                                            <img alt="Pokemon back" src={this.state.pokémon.sprites.back_default}/>
-                                        </div>}
-                                    </div>
-                                </div>
-{this.state.pokémon.sprites.back_default && <button onClick={this.toggleImage} className="button">↻</button>}
-                            </div>
+                                <dt><h4>Weight</h4></dt>
+                                <dd>{this.state.pokémon.weight / 10} kg</dd>
+                            </dl>
+                        </div>
+                        <div className="details-info-types">
+                            <h2>Types</h2>
+                            {this.state.pokémon.types.map((type) => {
+                                return <Link key={type.type.name} to={`/type/${type.type.name}`}
+                                        style={{backgroundColor: typeToColor(type.type.name)}} onClick={scrollToTop}
+                                        className="details-info-types-type">{capitalize(type.type.name)}</Link>})}
+                        </div>
+                        <div className="details-info-stats">
+                            <h2>Stats</h2>
+                            {this.state.pokémon.stats.map((stat) => {
+                                return <div className="details-info-stats-stat" key={stat.stat.name}>
+                                    {capitalize(stat.stat.name)}
+                                    <div style={{
+                                        width: `${stat.base_stat}px`,
+                                        backgroundColor: typeToColor(this.state.pokémon.types[0].type.name)
+                                    }}>{stat.base_stat}</div>
+                                </div>})}
                         </div>
                     </div>
-                    {this.props.match.path === "/random" &&
-                    <div className="button button-random" onClick={this.loadRandomPokémon}>Next →</div>}
-                    {this.state.caught ?
-                        <div className="button button-catch tooltip" data-tip={`Catch ${capitalize(this.state.pokémon.name)}!`}
-                             onClick={this.catchPokémon} style={{backgroundImage: `url(${pokéball_open})`}}/> :
-                        <div className="button button-catch tooltip" data-tip={`Let ${capitalize(this.state.pokémon.name)} go!`}
-                             onClick={this.catchPokémon} style={{backgroundImage: `url(${pokéball_closed})`}}/>}
-                </React.Fragment>
+                    <div className="details-image">
+                        <div id="flip-box" className={!this.state.pokémon.sprites.back_default ? 'disabled' : ''}>
+                            <div id="flip-box-inner">
+                                <div id="flip-box-front">
+                                    <img alt="Pokemon front" src={this.state.pokémon.sprites.front_default}/>
+                                </div>
+{this.state.pokémon.sprites.back_default && <div id="flip-box-back">
+                                                <img alt="Pokemon back" src={this.state.pokémon.sprites.back_default}/>
+                                            </div>}
+                            </div>
+                        </div>
+{this.state.pokémon.sprites.back_default && <button onClick={this.toggleImage} className="button">↻</button>}
+                    </div>
+                </div>
+            </div>
+{this.props.match.path === "/random" && <div className="button button-random" onClick={this.loadRandomPokémon}>Next →</div>}
+
+{this.state.caught ? <div className="button button-catch tooltip" data-tip={`Let ${capitalize(this.state.pokémon.name)} go!`}
+                     onClick={this.catchPokémon} style={{backgroundImage: `url(${pokéball_closed})`}}/> :
+                <div className="button button-catch tooltip" data-tip={`Catch ${capitalize(this.state.pokémon.name)}!`}
+                     onClick={this.catchPokémon} style={{backgroundImage: `url(${pokéball_open})`}}/>}
+        </React.Fragment>
                 }
             </React.Fragment>
         );
