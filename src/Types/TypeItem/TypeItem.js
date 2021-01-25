d798f1735fd9376af81e872a60033801f6e9423a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-01-25 01:05:10 +0100

    Converted TypeItem.js to functional component

--- src/Types/TypeItem/TypeItem.js
+++ src/Types/TypeItem/TypeItem.js
@@ -1,21 +1,19 @@
 import './TypeItem.scss'
-import React, {Component} from "react";
+import React from "react";
 import capitalize from "../../helpers/Capitalize";
 import typeToColor from "../../helpers/TypeToColor";
 import {Link} from "react-router-dom";
 
-class TypeItem extends Component {
-    render() {
-        return (
-            <Link to={`/type/${this.props.type.name}`}>
-                <div className="type">
-                    <div className="type-container" style={{backgroundColor: typeToColor(this.props.type.name)}}>
-                        <h3 className="type-name">{capitalize(this.props.type.name)}</h3>
-                    </div>
+function TypeItem(props) {
+    return (
+        <Link to={`/type/${props.type.name}`}>
+            <div className="type">
+                <div className="type-container" style={{backgroundColor: typeToColor(props.type.name)}}>
+                    <h3 className="type-name">{capitalize(props.type.name)}</h3>
                 </div>
-            </Link>
-        );
-    }
+            </div>
+        </Link>
+    );
 }
 
 export default TypeItem;
