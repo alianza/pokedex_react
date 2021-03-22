98a47e779091a6f10788dc8b7c78de99ec299b81
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-03-22 04:08:52 +0100

    Cleanup

--- src/helpers/services/CatchService.js
+++ src/helpers/services/CatchService.js
@@ -1,33 +1,29 @@
 const CatchService = {
-    add(pokémon)  {
-        let pokémons = this.getAll() || [];
+    add(pokémon)  { // Add to the array and save in local storage
+        let pokémons = this.getAll() || []; // Retrieve all
         pokémons.unshift(pokémon); // Add pokémon to beginning of array
-        localStorage.setItem('pokémons', JSON.stringify(pokémons));
+        localStorage.setItem('pokémons', JSON.stringify(pokémons)); // Save in local storage
     },
 
-    remove(pokémonName)  {
-        let pokémons = this.getAll() || [];
-        pokémons.forEach(function (pokémon, index) {
-            if (pokémon.name === pokémonName) {
-                pokémons.splice(index, 1);
-            }
+    remove(pokémonName)  { // Remove from the array and save in local storage
+        let pokémons = this.getAll() || []; // Retrieve all
+        pokémons.forEach(function (pokémon, index) { // Iterate though all
+            if (pokémon.name === pokémonName) { pokémons.splice(index, 1); } // Compare names, if match remove it
         });
-        localStorage.setItem('pokémons', JSON.stringify(pokémons));
+        localStorage.setItem('pokémons', JSON.stringify(pokémons)); // Save in local storage
     },
 
     get(pokémonName) {
         let pokémonToReturn;
-        let pokémons = this.getAll() || [];
-        pokémons.forEach(function (pokémon) {
-            if (pokémon.name === pokémonName) {
-                pokémonToReturn = pokémon;
-            }
+        let pokémons = this.getAll() || []; // Retrieve all
+        pokémons.forEach(function (pokémon) { // Iterate though all
+            if (pokémon.name === pokémonName) { pokémonToReturn = pokémon; } // Compare name, if match set as return value
         });
         return pokémonToReturn;
     },
 
     getAll() {
-        return JSON.parse(localStorage.getItem('pokémons'));
+        return JSON.parse(localStorage.getItem('pokémons')); // Retrieve all
     }
 }
 
