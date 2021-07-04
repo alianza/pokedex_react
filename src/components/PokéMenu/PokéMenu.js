0e88a97a51a04083cdc94e07f6bcd32677233d53
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-04 17:28:02 +0200

    Added auto closing mobile menu

--- src/components/PokéMenu/PokéMenu.js
+++ src/components/PokéMenu/PokéMenu.js
@@ -12,11 +12,17 @@ function PokéMenu(props) {
             'Made by Jan-Willem van Bremen - 2020');
     }
 
+    const closeMenuIfMobile = () => {
+        if (window.innerWidth < 600) {
+            document.getElementById('app').classList.remove('menu-active');
+        }
+    }
+
     return (
         <div className="menu">
             <div onClick={e => props.onMenuClick(e)} className="menu-close">✖</div>
             <h1>Menu</h1>
-            <ul className="menu-top">
+            <ul className="menu-top" onClick={() => closeMenuIfMobile()}>
                 <li><NavLink to={'/'} isActive={ (match) => pathname.includes('/page') || match.url === '/'}
                              activeClassName={'active'} onClick={() => scrollToTop()}>Home</NavLink></li>
                 <li><NavLink to={'/types'} isActive={ () => pathname.includes('/types') } activeClassName={'active'}>Types</NavLink></li>
